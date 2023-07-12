63e5d81469d724d100e358e38b65697ef137c5e7
miniOS <seungmin.oh.dev@gmail.com>
2023-07-12 18:12:34 +0900

    refactor: CardDeck을 다루기 위한 객체의 타입으로 struct에서 class로 변경

--- LuckyCardGame/LuckyCardGame/Model/LuckyCardDeck.swift
+++ LuckyCardGame/LuckyCardGame/Model/LuckyCardDeck.swift
@@ -12,13 +12,14 @@ import Foundation
  1. class로 구현할 이유가 없다고 생각했습니다.
     class로 구현하는 경우는 꼭 필요한 경우에만 해야한다고 생각합니다.
     이번 경우에는 상속을 필요로 하지도 않고, 값 복사에 대한 디메리트도 없고, deinit을 필요로 하지 않기 때문에 class로 구현의 근거가 없습니다.
+ 2. (0712추가) 리뷰를 받아 다시 생각해 봤을 때, 대부분의 메서드가 내부 프로퍼티를 조작하는 mutating 메서드이기 때문에, class로 구현하는 것도 좋을 것 같다고 판단했다.
  */
-struct LuckyCardDeck: CardDeckProtocol {
+class LuckyCardDeck: CardDeckProtocol {
     typealias CardType = LuckyCard
 
     private(set) var cards: [LuckyCard] = []
 
-    mutating func createShuffledCardDeck() {
+    func createShuffledCardDeck() {
         cards = LuckyCard.Animal.allCases.flatMap { animal in
             LuckyCard.Value.allCases.map { value in
                 return LuckyCard(animal: animal, value: value)
@@ -26,15 +27,15 @@ struct LuckyCardDeck: CardDeckProtocol {
         }.shuffled()
     }
 
-    mutating func filterCard(_ isIncluded: (LuckyCard) -> Bool) {
+    func filterCard(_ isIncluded: (LuckyCard) -> Bool) {
         cards = cards.filter(isIncluded)
     }
 
-    mutating func popLastCard() -> LuckyCard? {
+    func popLastCard() -> LuckyCard? {
         return cards.popLast()
     }
 
-    mutating func popCards(number: Int) -> [LuckyCard]? {
+    func popCards(number: Int) -> [LuckyCard]? {
         guard cards.count >= number else { return nil }
         var newCards = [LuckyCard]()
         for _ in 0..<number {
@@ -44,11 +45,11 @@ struct LuckyCardDeck: CardDeckProtocol {
         return newCards
     }
 
-    mutating func pushNewCard(_ newCard: LuckyCard) {
+    func pushNewCard(_ newCard: LuckyCard) {
         cards.append(newCard)
     }
 
-    mutating func pushCards(_ newCards: [LuckyCard]) {
+    func pushCards(_ newCards: [LuckyCard]) {
         cards.append(contentsOf: newCards)
     }
 }
