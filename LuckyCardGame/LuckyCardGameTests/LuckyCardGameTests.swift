ff2e2dae2ebfe17f26ad5ffcf773a15a32470843
miniOS <seungmin.oh.dev@gmail.com>
2023-07-12 23:16:00 +0900

    test: Game 초기화에 대한 TestCode작성

--- LuckyCardGame/LuckyCardGameTests/LuckyCardGameTests.swift
+++ LuckyCardGame/LuckyCardGameTests/LuckyCardGameTests.swift
@@ -6,30 +6,76 @@
 //
 
 import XCTest
+@testable import LuckyCardGame
 
 final class LuckyCardGameTests: XCTestCase {
 
+    var game: LuckyCardGame!
+    var playerCount: Int!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        game = LuckyCardGame()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        game = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_WhenPlayerNumberIs3() throws {
+        // given
+        playerCount = 3
+
+        // when
+        game.initGame(playerNumber: playerCount)
+
+        // then
+        XCTAssertEqual(game.dealerDeck.cards.count, 9)
+
+        XCTAssertFalse(game.playersDeck.allSatisfy({ playerDeck in
+            playerDeck.cards.contains(where: { card in
+                card.description.suffix(2) == "12"
+            })
+        }))
+
+        XCTAssertEqual(game.playersDeck.count, playerCount)
+        XCTAssertEqual(game.playersDeck[0].cards.count, 8)
+        XCTAssertEqual(game.playersDeck[1].cards.count, 8)
+        XCTAssertEqual(game.playersDeck[2].cards.count, 8)
+
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testInitiateGame_WhenPlayerNumberIs4() throws {
+        // given
+        playerCount = 4
+
+        // when
+        game.initGame(playerNumber: playerCount)
+
+        // then
+        XCTAssertEqual(game.dealerDeck.cards.count, 8)
+
+        XCTAssertEqual(game.playersDeck.count, playerCount)
+        XCTAssertEqual(game.playersDeck[0].cards.count, 7)
+        XCTAssertEqual(game.playersDeck[1].cards.count, 7)
+        XCTAssertEqual(game.playersDeck[2].cards.count, 7)
+        XCTAssertEqual(game.playersDeck[3].cards.count, 7)
+    }
+
+    func testInitiateGame_WhenPlayerNumberIs5() throws {
+        // given
+        playerCount = 5
+
+        // when
+        game.initGame(playerNumber: playerCount)
+
+        XCTAssertEqual(game.dealerDeck.cards.count, 6)
+
+        XCTAssertEqual(game.playersDeck.count, playerCount)
+        XCTAssertEqual(game.playersDeck[0].cards.count, 6)
+        XCTAssertEqual(game.playersDeck[1].cards.count, 6)
+        XCTAssertEqual(game.playersDeck[2].cards.count, 6)
+        XCTAssertEqual(game.playersDeck[3].cards.count, 6)
+        XCTAssertEqual(game.playersDeck[4].cards.count, 6)
     }
 
 }
